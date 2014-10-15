399a65259ce89bd6f48a8d9c20158b19f8198c37
Mario <mario.giancini@gmail.com>
2014-10-15 17:06:54 -0400

    Add fonts icons image sizes and functions
    Add fonts icons image sizes and functions

--- functions.php
+++ functions.php
@@ -2,16 +2,20 @@
 //* Start the engine
 include_once( get_template_directory() . '/lib/init.php' );
 
+include_once( get_stylesheet_directory()  . '/lib/theme-helpers.php' );
+
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'The DBZ Official Theme' );
 define( 'CHILD_THEME_URL', 'http://thedbz.com/' );
 define( 'CHILD_THEME_VERSION', '2.1.2' );
 
-//* Enqueue Google Fonts
+//* Enqueue Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+	
+	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
 
 }
 
@@ -30,10 +34,10 @@ add_theme_support( 'genesis-structural-wraps', array(
 add_theme_support(
   'genesis-menus', 
 	array(
-		'primary'   => __( 'Primary Navigation Menu', 'child-domain' ),
-		'secondary' => __( 'Secondary Navigation Menu', 'child-domain' ), 
-		'top'       => __( 'Top Navigation Menu', 'child-domain' ),
-		'footer'	=> __( 'Footer Navigation Menu', 'child-domain' )
+		'primary'   => __( 'Primary Navigation Menu', 'thedbz' ),
+		'secondary' => __( 'Secondary Navigation Menu', 'thedbz' ), 
+		'top'       => __( 'Top Navigation Menu', 'thedbz' ),
+		'footer'	=> __( 'Footer Navigation Menu', 'thedbz' )
 	)
 );
 
@@ -47,16 +51,11 @@ add_theme_support( 'post-formats', array(
 	'video'
 ) );
 
-/**
- * Utilize Image Post Format to display featured image
- */
-add_action( 'genesis_before_entry_content', 'post_format_image_featured' );
-function post_format_image_featured() {
-	if ( has_post_format( 'image' ) && has_post_thumbnail() && is_singular( 'post' ) ) { 
-		$img = genesis_get_image( array( 'format' => 'html', 'size' => genesis_get_option( 'image_size' ), 'attr' => array( 'class' => 'post-image' ) ) );
-		printf( '<a href="%s" id="featured-post-image" title="%s">%s</a>', get_permalink(), the_title_attribute( 'echo=0' ), $img );
-	}
-}
+//* Add new image sizes
+add_image_size('dbz-grid', 400, 200, TRUE);
+add_image_size('dbz-medium', 600, 300, TRUE);
+add_image_size('dbz-large', 800, 400, TRUE);
+add_image_size('dbz-xl', 1200, 600, TRUE);
 
 //* Add HTML5 markup structure
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
@@ -69,3 +68,49 @@ add_theme_support( 'custom-background' );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Allow shortcodes to work in text widget
+add_filter('widget_text', 'do_shortcode');
+
+/**
+ * Utilize Image Post Format to display featured image on single post
+ */
+add_action( 'genesis_before_entry_content', 'post_format_image_featured' );
+function post_format_image_featured() {
+	if ( has_post_format( 'image' ) && has_post_thumbnail() && is_singular( 'post' ) ) { 
+		$img = genesis_get_image( array( 'format' => 'html', 'size' => genesis_get_option( 'image_size' ), 'attr' => array( 'class' => 'post-image' ) ) );
+		printf( '<a href="%s" id="featured-post-image" title="%s">%s</a>', get_permalink(), the_title_attribute( 'echo=0' ), $img );
+	}
+}
+
+
+/**
+ * Custom entry meta in entry header - remove "by" & "edit"
+ */
+add_filter( 'genesis_post_info', 'dbz_post_info_filter' );
+function dbz_post_info_filter($post_info) {
+	
+	if( is_singular() ) {
+		$post_info = '[post_date_human_time] [post_author_posts_link]';
+	    return $post_info;
+	} else {
+		$post_info = '[post_date_human_time] [post_categories before=""]';
+    	return $post_info;
+	}
+
+}
+ 
+/**
+ * Custom post meta in entry footer - remove text
+ */
+add_filter( 'genesis_post_meta', 'dbz_filter_post_meta' );
+function dbz_filter_post_meta($post_meta) {
+    
+	if( is_singular() ) {
+		$post_meta = '[post_categories before=""] [post_tags before=""]';
+    	return $post_meta;
+	} else {
+		$post_meta = ''; // removed tags: [post_tags before=""]
+    	return $post_meta;
+	}
+}
